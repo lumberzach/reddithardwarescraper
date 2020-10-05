b2c24825899cb21ef650914ecfbc558d2da797f0
Zach <boxcarracer0110@gmail.com>
2020-10-04 21:48:23 -0700

    corrected loop to only add new results to google sheets, commented code

--- hardwarescraper.py
+++ hardwarescraper.py
@@ -1,17 +1,46 @@
 import praw
+import gspread
+from dotenv import load_dotenv
+import os
 
-reddit = praw.Reddit(client_id='P3FtIvElljRLtg', client_secret='QQ18Nf-O9q8VWmqAR-pEhpThPr8', user_agent='hardwareswapscraper')
+
+# Dotenv
+load_dotenv()
+clientcreds = os.getenv('client_creds')
+secretcreds = os.getenv('secret_creds')
 
 
-# while True:
-new_posts = reddit.subreddit('hardwareswap').new(limit=10)
-item = "asus"
+# PRAW
+reddit = praw.Reddit(client_id=clientcreds, client_secret=secretcreds, user_agent='hardwareswapscraper')
+
+
+# Google Spreadsheets auth
+scope = ['https://spreadsheets.google.com/feeds']
+gc = gspread.service_account(filename='client_secret.json')
+sh = gc.open_by_key("1Wch_VKB2Hop_QtRC0_0VLuMxlYSrZ6Ac5CHLHSNa42U")  # ID in URL of sheet
+sheet = sh.sheet1
+
+
+# Setting empty string to append results to
 results = []
-for post in new_posts:
-    # print(post.title)
-    if item in post.title.lower():
-        if post.title.lower() not in results:
-            results.append(post.title)
-            print(results[-1])
+
+
+# Item we are searching for
+item = "paypal"
+
+
+# Checking the 10 newest posts and printing any matches for the item
+while True:
+    new_posts = reddit.subreddit('hardwareswap').new(limit=10)
+    for post in new_posts:
+        if item in post.title.lower():
+            if post.title not in results:
+                results.append(post.title)
+                row = [post.title]
+                index = 1
+                sheet.insert_row(row, index)
+                print(post.title)
+
+
             
 
